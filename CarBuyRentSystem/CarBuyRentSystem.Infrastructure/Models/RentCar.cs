a72a227d8e71beb8aad5200c647824ad482475d7
Ferdi Mehmedov <FredericCo@mail.bg>
2022-03-05 13:53:24 +0200

    Create Entity RentCar

--- CarBuyRentSystem/CarBuyRentSystem.Infrastructure/Models/RentCar.cs
+++ CarBuyRentSystem/CarBuyRentSystem.Infrastructure/Models/RentCar.cs
@@ -1,6 +1,22 @@
 ﻿namespace CarBuyRentSystem.Infrastructure.Models
 {
+    using System;
     public class RentCar
     {
+        public int RentCarId { get; set; }
+
+        public string UserId { get; set; }
+
+        public User User { get; set; }
+
+        public int CarId { get; set; }
+
+        public Car Car { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public decimal TotalPrice { get; set; }
     }
 }
