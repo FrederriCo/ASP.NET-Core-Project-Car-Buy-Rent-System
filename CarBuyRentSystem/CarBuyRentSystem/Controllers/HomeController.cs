9007e40ef5f54f3e0fd3733926d7e92c860add86
Ferdi Mehmedov <FredericCo@mail.bg>
2022-03-25 10:20:05 +0200

    Create Application Error page

--- CarBuyRentSystem/CarBuyRentSystem/Controllers/HomeController.cs
+++ CarBuyRentSystem/CarBuyRentSystem/Controllers/HomeController.cs
@@ -50,6 +50,10 @@
             return View(cars);
 
         }
+        public IActionResult ApplicationError()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error() => View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
--- CarBuyRentSystem/CarBuyRentSystem/Controllers/RentCarsController.cs
+++ CarBuyRentSystem/CarBuyRentSystem/Controllers/RentCarsController.cs
@@ -85,6 +85,5 @@
             return this.View(rentedCars);
         }
 
-
     }
 }
